e9af775c4e26a74ffc14baa9f84a7fe021b0f208
Davide Buin <davidebuin@Mac.fritz.box>
2025-07-02 10:31:21 +0200

    Fix flake8 E402 for sys.path in test_model

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,23 @@
-import torch
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            '..',
+            'code'
+        )
+    )
+)
+
+
 from model import SimpleMLP
+import torch
 
 
 def test_model_forward_pass():
     model = SimpleMLP()
     dummy_input = torch.randn(1, 1, 28, 28)
     output = model(dummy_input)
-    assert output.shape == (1, 10)
+    assert output.shape == (1, 10)  # Deve avere 10 output, uno per classe
